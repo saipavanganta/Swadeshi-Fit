8a8d43de45239c2bb190a15f3b32cc21c0adc395
Harsh-2004-ux <harshendrasingh17@gmail.com>
2026-03-15 14:11:53 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN || "https://swadeshi-fit-frontend1.onrender.com",
     credentials: true,
   })
 );
