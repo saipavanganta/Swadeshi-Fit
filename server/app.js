621796e0dbe9bc7910e26d92cb214e9ceb4b6cc4
Harsh-2004-ux <harshendrasingh17@gmail.com>
2026-03-15 14:25:33 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,12 +3,13 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN || "https://swadeshi-fit-frontend1.onrender.com",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://swadeshi-fit-frontend1.onrender.com"
+  ],
+  credentials: true
+}));
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
